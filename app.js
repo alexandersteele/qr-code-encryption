4394ad97ec477cad5c0cfde69c7012ce505e32b9
Alexander Steele <alexander.steele@and.digital>
2020-03-09 15:22:26 +0000

    added promises to decrypt and encrypt to allow for async design

--- app.js
+++ app.js
@@ -1,24 +1,43 @@
+const QRCode = require('qrcode')
+const QRCodeReader = require('qrcode-reader');
+const Jimp = require("jimp");
+const fs = require('fs');
+const CryptoJS = require("crypto-js");
 
-var QRCode = require('qrcode')
-var QrCode = require('qrcode-reader');
-var Jimp = require("jimp");
-var fs = require('fs');
 
-QRCode.toFile('code_images/code.png', 'hi brad', async() => {
-    const qrData = await readQR();
-    console.log(qrData.result);
-})
+const createEncrpytedMessage = (input, secret) => CryptoJS.AES.encrypt(input, secret).toString();
 
-const readQR = async () => {
-    var buffer = fs.readFileSync('code_images/code.png');
+const readQR = async (file) => {
+    var buffer = fs.readFileSync(file);
     const image = await Jimp.read(buffer);
-    const qr = new QrCode();
+    const qr = new QRCodeReader();
     const resultPromise = new Promise((res, rej) => {
-        qr.callback = (err, result) => {
-            res(result);
-        };
+        qr.callback = (err, result) => res(result);
         qr.decode(image.bitmap);
     });
-    const result = await resultPromise;
-    return result;
+    return await resultPromise;
 };
+
+module.exports = encryptQR = (pathname, message, secret) => {
+    return new Promise ((res, rej) => {
+        try {
+            res(QRCode.toFile(pathname, createEncrpytedMessage(message, secret)))
+        } catch (e) {
+            rej(e)
+        }
+    })     
+}
+
+module.exports = decryptQR = (pathname, secret) => {
+    return new Promise ((res, rej) => {
+        readQR(pathname)
+        .then((qrData) => (
+            res(CryptoJS.AES.decrypt(qrData.result, secret).toString(CryptoJS.enc.Utf8)))
+        )
+        .catch(e => rej(e))
+    })
+}
+
+encryptQR('code_images/encrypted_code.png', "hi bradley", "abcdef").then(() => {
+    decryptQR('code_images/encrypted_code.png', "abcdef").then((x) => console.log(x)) 
+})
--- src/generateQR.js
+++ src/generateQR.js
@@ -1,12 +1,1 @@
 
-
-module.exports = generateQR = async input => {
-
-    await 
-    return 'success';
-    
-  }
-
-// module.exports = generateQR = (input) => (
-//     QRCode.toFile('code_images/code.png', input, () => console.log('Code generated'))
-// );
